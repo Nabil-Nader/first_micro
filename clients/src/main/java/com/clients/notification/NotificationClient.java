76ae1ef913e4fa9e27756add0e3b173900211ccb
nabil <nabil.nader00@gmail.com>
2022-08-27 21:19:47 +0200

    add notification layer

--- clients/src/main/java/com/clients/notification/NotificationClient.java
+++ clients/src/main/java/com/clients/notification/NotificationClient.java
@@ -1,9 +1,6 @@
 package com.clients.notification;
 
-import com.clients.fraud.FraudCheckResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
