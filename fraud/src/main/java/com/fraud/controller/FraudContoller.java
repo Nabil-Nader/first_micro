2e6f53dc576fa64361db312ae65a1dbaeb6f7bbf
nabil <nabil.nader00@gmail.com>
2023-04-22 05:32:15 +0200

    fix fraud controller

--- fraud/src/main/java/com/fraud/controller/FraudContoller.java
+++ fraud/src/main/java/com/fraud/controller/FraudContoller.java
@@ -4,7 +4,10 @@ import com.clients.fraud.FraudCheckResponse;
 import com.fraud.service.FraudCheckService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fraud-check")
@@ -15,13 +18,13 @@ public class FraudContoller {
     private final FraudCheckService fraudCheckService;
 
     @GetMapping(path = "/{customerId}")
-    public FraudCheckResponse isFraudster(
-            @PathVariable("customerId") Long customerId) {
-        boolean isFraudulentCustomer = fraudCheckService.
-                isFraudulentCustomer(customerId);
+    public FraudCheckResponse isFraudster(@PathVariable("customerId") Long customerId) {
+
         log.info("fraud check request for customer {}", customerId);
+        boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
 
         return new FraudCheckResponse(isFraudulentCustomer);
     }
+
 }
 
