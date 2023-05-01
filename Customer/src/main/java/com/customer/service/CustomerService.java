e4b617c4d9d0d5c0d0dc0d9e793b7766c758379e
nabil.nader <nabil.nader00@gmail.com>
2023-05-01 12:56:44 +0300

    done with rabbitMq

--- Customer/src/main/java/com/customer/service/CustomerService.java
+++ Customer/src/main/java/com/customer/service/CustomerService.java
@@ -1,5 +1,6 @@
 package com.customer.service;
 
+import com.amqp.config.RabbitMQMessageProducer;
 import com.clients.fraud.FraudCheckResponse;
 import com.clients.fraud.FraudClient;
 import com.clients.notification.NotificationClient;
@@ -17,7 +18,7 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final FraudClient fraudClient;
-    private final NotificationClient notificationClient;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.getFirstName())
@@ -34,15 +35,27 @@ public class CustomerService {
             throw new IllegalStateException("fraudster");
         }
 
-        // todo: make it async. i.e add to queue
-        notificationClient.sendNotification(
+        // once we add RabbitMQ, we no longer need this
+
+        // to do make it async. i.e add to queue
+        /*notificationClient.sendNotification(
                 new NotificationRequest(
                         customer.getId(),
                         customer.getEmail(),
                         String.format("Hi %s, welcome to Amigoscode...",
                                 customer.getFirstName())
                 )
-        );
+        );*/
+
+        NotificationRequest notificationRequest = new NotificationRequest(
+                customer.getId(),
+                customer.getEmail(),
+                String.format("Hi %s, welcome to Amigoscode...",
+                        customer.getFirstName()));
+
+        rabbitMQMessageProducer.publish(notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key");
 
     }
 }
--- Notification/src/main/java/com/ex/NotificationApplication.java
+++ Notification/src/main/java/com/ex/NotificationApplication.java
@@ -2,13 +2,28 @@ package com.ex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ex", "com.amqp"})
 public class NotificationApplication {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(NotificationApplication.class,args);
+        SpringApplication.run(NotificationApplication.class, args);
     }
+
+   /*     @Bean
+        CommandLineRunner commandLineRunner(
+            RabbitMQMessageProducer producer,
+            NotificationConfig notificationConfig
+            ) {
+        return args -> {
+            producer.publish(
+                    new Person("Ali", 18),
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey());
+        };
+    }
+
+    record Person(String name, int age){}*/
+
 }
